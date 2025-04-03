756f3df0385b38c9c74f15397c3f398c62497d88
Nay <nay.fullstack@gmail.com>
2025-04-03 08:59:19 +0200

    ultimos retoques, sin poder conseguir el push

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+/*Primero capturamos el form y el añadimos un evento de subida*/
 document.getElementById("form").addEventListener("submit", function(event) {
+
+    /*let users = JSON.parse(localStorage.getitem("users")) || []*/
+
     event.preventDefault();
+    /*metemos los valores de los input en una variable*/
     let user = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -7,14 +12,18 @@ document.getElementById("form").addEventListener("submit", function(event) {
         image: document.getElementById("image").value,
     }
     
+    /*users.push(user)*/
+    
+    /*subimos el user al localstorage como un string en Json*/
     localStorage.setItem("user", JSON.stringify(user));
-
+    /*aqui lo volvemos a recuperar y lo metemos en una variable*/
     const UserInfo = JSON.parse(localStorage.getItem("user"));
+   /*se crea un parrafo con la info subida al localstorage debajo del form, se guarda en una variable y despues se añade la info*/
     let UserInfoparagraph = document.createElement("p");    
     UserInfoparagraph.innerHTML = `HOLA ${UserInfo.name} <br> EMAIL: ${UserInfo.email} <br> MENSAJE: ${UserInfo.message} <br> IMAGEN: ${UserInfo.image}`;
     document.body.appendChild(UserInfoparagraph);
 });
-
+/*capturamos el boton de borrar y le añadimos un evento de click para el borrado de datos del localstorage con clear*/
 document.getElementById("clear").addEventListener("click", function() {
     localStorage.clear();
 });
